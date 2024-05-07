8a6f15fb7f9c9f0f74b5cf5adfdabe39dbb40d04
amine znaki <znakia21@gmail.com>
2024-05-07 11:09:59 +0100

    Hash passwords using bcrypt for user registration

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Import necessary modules
+import bcrypt
 from flask import Flask, render_template, request
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -6,6 +7,7 @@ from firebase_admin import credentials, firestore
 # Flask application instance
 app = Flask(__name__)
 
+# Initialize Firebase Admin SDK with service account credentials
 cred = credentials.Certificate("C:\\Users\\znaki\\OneDrive\\Desktop\\authenticationsystem.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
@@ -15,21 +17,25 @@ db = firestore.client()
 def index():
     return render_template('index.html')
 
+# Define a route for user registration
 @app.route('/register', methods=['POST'])
 def register():
     username = request.form['username']
     email = request.form['email']
     password = request.form['password']
 
-    user_ref = db.collection('users').document()
-    user_ref.set({
+    # Hash the password using bcrypt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    # Store user data in Firestore
+    users_ref = db.collection('users')
+    users_ref.add({
         'username': username,
         'email': email,
-        'password': password
+        'password': hashed_password.decode('utf-8')  # Decode hashed password to store as string
     })
+
     return "User registered successfully!"
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
